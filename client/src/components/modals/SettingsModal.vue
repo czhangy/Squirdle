3908df8250cc486fa9d5ae5683b7181b90a26e52
Charles Zhang <charleszhangmb@gmail.com>
2022-03-14 17:36:13 -0700

    Credits

--- client/src/components/modals/SettingsModal.vue
+++ client/src/components/modals/SettingsModal.vue
@@ -5,6 +5,29 @@
 			<p class="settings-text">
 				lol imagine if i had any settings to implement
 			</p>
+			<hr />
+			<p class="settings-text">Check me out at:</p>
+			<div id="settings-links">
+				<a href="https://github.com/czhangy" target="_blank"
+					><img src="@/assets/icons/github.png" href="GitHub"
+				/></a>
+                <a href="https://www.linkedin.com/in/charles-zhang-14746519b/" target="_blank"
+					><img src="@/assets/icons/linkedin.png" href="LinkedIn"
+				/></a>
+                <a href="https://czhangy.io" target="_blank"
+					><img src="@/assets/icons/personal-site.png" href="Personal Site"
+				/></a>
+			</div>
+			<hr />
+			<p class="settings-text">
+				This app was created with the help of
+				<br />
+				<a href="https://serebii.net" target="_blank">Serebii</a>,
+				<a href="https://pokeapi.co/" target="_blank">PokeAPI</a>, and
+				<a href="https://msikma.github.io/pokesprite/" target="_blank"
+					>PokeSprite</a
+				>
+			</p>
 		</div>
 	</Modal>
 </template>
@@ -28,25 +51,42 @@ export default {
 
 <style lang="scss" scoped>
 #settings {
-	// Sizing
 	height: 100%;
 	width: 100%;
 
 	#settings-header {
-		// Typography
 		font-family: $alt-font;
 		font-size: 1.5rem;
 		line-height: 1.5rem;
 		letter-spacing: 2px;
 		color: white;
 		text-decoration: underline;
-		// Spacing
 		margin-bottom: 16px;
 	}
 
 	.settings-text {
-		// Typography
 		color: white;
+
+		a {
+			color: white;
+		}
+	}
+
+	hr {
+		height: 2px;
+		background: white;
+		border: none;
+		margin: 16px 0;
+	}
+
+	#settings-links {
+        margin-top: 16px;
+        display: flex;
+        justify-content: space-evenly;
+
+        img {
+            height: 32px;
+        }
 	}
 }
 </style>
