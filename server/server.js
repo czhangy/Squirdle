fd6df7a920f2202309abb3de102e6c0739fbe866
Charles Zhang <charleszhangmb@gmail.com>
2022-03-15 23:53:06 -0700

    Test

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ mongoose
 app.use((req, res, next) => {
 	if (process.env.NODE_ENV === "production") {
 		if (req.headers["x-forwarded-proto"] !== "https")
-			return res.redirect("https://" + req.headers.host + req.url);
+			return res.redirect("https://" + req.headers.host);
 		else return next();
 	} else return next();
 });
